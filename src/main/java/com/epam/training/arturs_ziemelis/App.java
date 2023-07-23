809bbb4d02e1cc04577b67a5e1041cab55b5dd21
Arturik Live <arturiklive@gmail.com>
2023-07-23 18:05:15 +0300

    Second test commit

--- src/main/java/com/epam/training/arturs_ziemelis/App.java
+++ src/main/java/com/epam/training/arturs_ziemelis/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(Sum(1,2));
+    }
+    public static int Sum(int a, int b){
+        return a + b;
     }
 }
